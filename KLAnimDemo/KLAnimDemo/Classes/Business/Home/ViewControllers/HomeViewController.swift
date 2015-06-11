8205ea2783b0dfdd791666ab21459211e54d6213
huhood <jakend@163.com>
2015-06-11 10:15:35 +0800

    初始化导入
    初始化项目框架

---
+++ KLAnimDemo/KLAnimDemo/Classes/Business/Home/ViewControllers/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HomeViewController.swift
+//  KLAnimDemo
+//
+//  Created by 浩 胡 on 15/6/11.
+//  Copyright (c) 2015年 siyan. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ KLAnimDemo/KLAnimDemo/Classes/Business/Home/ViewControllers/MineViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MineViewController.swift
+//  KLAnimDemo
+//
+//  Created by 浩 胡 on 15/6/11.
+//  Copyright (c) 2015年 siyan. All rights reserved.
+//
+
+import UIKit
+
+class MineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
