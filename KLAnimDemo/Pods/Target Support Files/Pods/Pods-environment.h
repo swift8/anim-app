b33593bab2a00f5b0119deef5681b929a234f06c
huhood <jakend@163.com>
2015-06-11 15:23:24 +0800

    加入数据库类

--- KLAnimDemo/Pods/Target Support Files/Pods/Pods-environment.h
+++ KLAnimDemo/Pods/Target Support Files/Pods/Pods-environment.h
@@ -12,6 +12,27 @@
 #define COCOAPODS_VERSION_MINOR_Alamofire 2
 #define COCOAPODS_VERSION_PATCH_Alamofire 2
 
+// SQLite.swift
+#define COCOAPODS_POD_AVAILABLE_SQLite_swift
+// This library does not follow semantic-versioning,
+// so we were not able to define version macros.
+// Please contact the author.
+// Version: 0.1.0.pre.
+
+// SQLite.swift/Core
+#define COCOAPODS_POD_AVAILABLE_SQLite_swift_Core
+// This library does not follow semantic-versioning,
+// so we were not able to define version macros.
+// Please contact the author.
+// Version: 0.1.0.pre.
+
+// SQLite.swift/Library
+#define COCOAPODS_POD_AVAILABLE_SQLite_swift_Library
+// This library does not follow semantic-versioning,
+// so we were not able to define version macros.
+// Please contact the author.
+// Version: 0.1.0.pre.
+
 // SwiftyJSON
 #define COCOAPODS_POD_AVAILABLE_SwiftyJSON
 #define COCOAPODS_VERSION_MAJOR_SwiftyJSON 2
